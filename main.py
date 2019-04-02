adc28f773563878461c1ab3563aa34df5ac5919a
Mira Coro <mirarat@gmail.com>
2019-04-02 19:37:52 +0300

    Connect to bd function

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 import logging
 import wtdb
+import pickle
+import sqlite3
 
+html_page = str(input("Please specify a site name: \n"))
 
-html_page = input("Please specify a site name: \n")
+#print(page)
 #html_page = requests.get(page)
 tag_count = 0
 dict = {}
@@ -22,7 +25,8 @@ for tag in ts:
 print(dict)
 
 
-site_name = (soup.find('title')).contents[0]
+site_name = (soup.find('title'))
+site_name = site_name.contents[0]
 print (site_name)
 
 current_date = (datetime.datetime.now()).strftime("%Y-%m-%d")
@@ -43,6 +47,27 @@ logger.addHandler(fh)
 logger.info(site_name)
 base = "mybd.db"
 
+bdict = pickle.dumps(dict)
 
-wtdb.write_to_db(dict, base)
-xcfkkl
+
+
+
+def write_to_db(bd):
+    #from main import site_name, page_name, current_date, bdict
+    page = ''
+    page = html_page
+    conn = sqlite3.connect(bd)
+    cursor = conn.cursor()
+
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Tags_of_sites
+                  (site_name text, url text, date text, tags blob)''')
+
+    cursor.execute('''INSERT INTO Tags_of_sites VALUES (?, ?, ?, ?)''', (site_name, page, current_date, bdict))
+
+#cursor.execute('INSERT INTO Tag_of_sites VALUES (?, ?)', (id, pickle.dumps(dict)))
+    conn.commit()
+    conn.close()
+    return bd
+
+wtdb.write_to_db(base)
--- wtdb.py
+++ wtdb.py
@@ -1,11 +1,11 @@
-import pickle
+
 import sqlite3
 
 
 
-def write_to_db(dct, bd):
-    from main import site_name, html_page, current_date
-    bdict = pickle.dumps(dct)
+def write_to_db(bd):
+    from main import site_name, page_name, current_date, bdict
+
 
     conn = sqlite3.connect(bd)
     cursor = conn.cursor()
@@ -14,9 +14,9 @@ def write_to_db(dct, bd):
     cursor.execute('''CREATE TABLE IF NOT EXISTS Tags_of_sites
                   (site_name text, url text, date text, tags blob)''')
 
-    cursor.execute('''INSERT INTO Tags_of_sites VALUES (?, ?, ?, ?)''', (site_name, html_page, current_date, bdict))
+    cursor.execute('''INSERT INTO Tags_of_sites VALUES (?, ?, ?, ?)''', (site_name, page_name, current_date, bdict))
 
 #cursor.execute('INSERT INTO Tag_of_sites VALUES (?, ?)', (id, pickle.dumps(dict)))
     conn.commit()
     conn.close()
-    return bd
+    #return bd
