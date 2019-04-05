fa14e866bb4023149e89266257773c8bafab9e66
Mira Coro <mirarat@gmail.com>
2019-04-05 20:14:14 +0300

    GUI almost done

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import datetime
 from bs4 import BeautifulSoup
 import requests
 import logging
-import wtdb
 import pickle
 import sqlite3
-import tkinter
+from tkinter import *
 import re
-
 
 current_date = (datetime.datetime.now()).strftime("%Y-%m-%d")
 base = "mybd.db"
-
 
 # html_page = input("Please specify a site name: \n")
 # html_page = requests.get(page)
@@ -44,7 +41,6 @@ def tagcounter(html_page):
 
     cursor.execute('''INSERT INTO Tags_of_sites VALUES (?, ?, ?, ?)''', (site_name, html_page, current_date, bdict))
 
-    # cursor.execute('INSERT INTO Tag_of_sites VALUES (?, ?)', (id, pickle.dumps(dict)))
     conn.commit()
     conn.close()
 
@@ -61,30 +57,63 @@ def tagcounter(html_page):
     logger.addHandler(fh)
 
     logger.info(site_name)
-    # print(list_of_tags)
     return list
-
 
 list_of_tags = tagcounter('https://bbc.com')
 print(list_of_tags)
 
+root = Tk()
+List_of_sites = Listbox(root, selectbackground='blue', selectforeground='yellow', text="List of sites", selectmode=SINGLE)
 
-
-root = tkinter()
-List = Listbox(root, selectbackground='blue', selectforeground='yellow', text="List of sites", selectmode=SINGLE)
-config = open(synonyms.yml,r)
-for site in config:
-    line = config.readline()
-    line = re.search("https://.+", line).group(0)
+with open('synonyms.yml', 'r') as config:
+    for site in config:
+        line = config.readline()
+        line = re.search("https://.+", line).group(0)
     #print (line.group(0))
-    List.insert(END, line)
+        List_of_sites.insert(END, line)
 
+
+List_of_sites.pack(side=LEFT, fill=BOTH, expand=1)
+
+Select_button = Button(root, text="Select a site", command=lambda: tagcounter(List_of_sites.get(ACTIVE)))
+Select_button.pack()
+
+list_of_synonyms = Listbox(root, selectbackground='blue', selectforeground='yellow', text="List of synonyms ", selectmode=SINGLE)
+
+for syn in config:
+    line = config.readline()
+    list_of_synonyms.insert(END, line)
+
+
+
+list_of_synonyms.pack(side=RIGHT, fill=BOTH, expand=1)
+
+Delete_button = Button(root, command=lambda list_of_synonyms=list_of_synonyms: list_of_synonyms.delete(ACTIVE), text="Delete a site from synonyms's list")
+Delete_button.pack(side=RIGHT)
+
+config = open('synonyms.yml', 'a')
+v = StringVar().set(input("Please specify synonym and site name: \n"))
+Enter_syn_site = Entry(root, width=20, textvariable=v)
+Enter_syn_site.pack()
+Add_button = Button(root, text="Add a site to synonyms's list", command=config.write(v))
+Add_button.pack(side=LEFT)
+
+Show_from_db_button = Button(root, text="Show tags from database", command=) #запрос и раскодирование
+Show_from_db_button.pack(side=RIGHT)
+
+w = StringVar().set(input("Please specify a site name: \n"))
+Enter_site = Entry(root, width=20, textvariable=w)
+Enter_site.pack()
+
+Get_tags_button = Button(root, text="Show tags from database", command=tagcounter(w))
+Get_tags_button.pack(side=RIGHT)
+
+Tags = Label(root, bg='black', fg='white', width=60)
+Tags.pack()
+
+Status = Label(root, bg='white', fg='green', width=60)
+Status.pack()
 config.close()
-
-config = open('synonyms.yml', 'r')
-
-
-
 root.mainloop()
 
 
--- temp1.py
+++ temp1.py
@@ -1,11 +1,19 @@
-import re
+from tkinter import *
+root = Tk()
+List_of_sites = Listbox(root, selectbackground='blue', selectforeground='yellow', text="List of sites", selectmode=SINGLE)
 
 config = open('synonyms.yml', 'r')
-
 for site in config:
-    #print(type(site))
-    exp = 'https://.+'
-    line = re.search('https://.+', site)
-    print (line.group(0))
+    line = config.readline()
+    line = re.search("https://.+", line).group(0)
+    #print (line.group(0))
+    List_of_sites.insert(END, line)
+
+Site = List_of_sites.get(ACTIVE)
 
 config.close()
+List_of_sites.pack(side=LEFT, fill=BOTH, expand=1)
+
+Select_button = Button(root, text="Select a site", command=lambda tagcounter(Site))
+Select_button.pack()
+root.mainloop()
